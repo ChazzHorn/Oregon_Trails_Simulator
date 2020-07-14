d3b96fb5765bf6a461873dc701f6a8863843e125
Chazz Horn <Chazzhorn1@gmail.com>
2020-07-14 11:45:12 -0400

    started extra features for OT & changed getAvailableSeatCount()

--- main.js
+++ main.js
@@ -1,3 +1,37 @@
+
+
+const canvas = document.getElementById('canvas')
+const oTPic = document.createElement('img');
+oTPic.setAttribute('src','images/OT_PIC.jpeg')
+canvas.appendChild(oTPic)
+
+const instructBar = document.createElement('div')
+
+canvas.appendChild(instructBar)
+instructBar.style.border = '2px solid rgb(255, 248, 220)'
+instructBar.style.height = '100px'
+
+const actionBar = document.createElement('div')
+
+canvas.appendChild(actionBar)
+
+actionBar.style.border = '2px solid rgb(255, 248, 220)'
+actionBar.style.fontSize = '20px'
+actionBar.style.height = '180px'
+
+let inputCapacity = document.createElement('input')
+
+instructBar.appendChild(inputCapacity)
+
+inputCapacity.style.width = '400px'
+inputCapacity.style.height = '25px'
+inputCapacity.style.fontSize = '25px'
+inputCapacity.style.bottom = 0
+
+
+
+
+
 class Traveler {
     constructor (name){
         this.name = name
@@ -27,11 +61,9 @@ class Wagon {
         this.passengers = []
     }
 
-    getAvailableSeatCount(){
-        if (this.passengers.length <= 2){
+    getAvailableSeatCount(){//I changed this method
+        if (this.passengers.length <= this.capacity){
             return this.capacity - this.passengers.length
-        }else{
-            return 0
         }
     }
 
@@ -39,6 +71,9 @@ class Wagon {
         if (this.getAvailableSeatCount()== 0){
            return 'no space left' 
         }else{
+           
+            actionBar.append(JSON.stringify(traveler))
+            
         return this.passengers.push(traveler)
         }
     }
@@ -60,3 +95,4 @@ class Wagon {
     
     
 }
+
